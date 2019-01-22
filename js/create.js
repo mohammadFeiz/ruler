d3f0b4e5da9b08cda12c7265b60ed648a206fb4e
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2019-01-23 01:36:24 +0330

    .

--- js/create.js
+++ js/create.js
@@ -11,7 +11,7 @@
         getMode: function () { return this.state.mode; },
         getLines: function () {
             var s = this.state, lines = [], points = this.getPoints();
-            if (points.length < 2) { return [];}//در حالتی که یک پوینت بیشتر موجود نیست لاینی نباید تولید شود این مشکل در حالت کلوز ترو خود را نشان می دهد
+            if (points.length < 2) { return []; }//در حالتی که یک پوینت بیشتر موجود نیست لاینی نباید تولید شود این مشکل در حالت کلوز ترو خود را نشان می دهد
             var length = points.length + (s.close ? 1 : 0);
             for (var i = 1; i < length; i++) {
                 var lastPoint = points[i - 1], point = points[i] || points[0];
@@ -33,11 +33,11 @@
     return a;
 }
 var create = {
-    drawing:false,
+    drawing: false,
     firstPoint: true,
     ngonSides: 6,
     ortho: true,
-    snapArea:15,
+    snapArea: 15,
     mousedown: function (e) {
         app.eventHandler("window", "mousemove", $.proxy(this.mousemove, this));
         app.eventHandler("window", "mouseup", $.proxy(this.mouseup, this));
@@ -45,10 +45,10 @@ var create = {
         if (this.firstPoint) {
             this.object = new spline({ start: coords, color: layers.getActive().color, mode: mode, sides: this.ngonSides, ortho: this.ortho, close: close }, this.getPoints[mode]);
             this.firstPoint = false;
-            this.drawing=true;
+            this.drawing = true;
         } else {
             this.object.addPoint(coords);
-            if (close) { this.firstPoint = true; }
+            if (close) { this.firstPoint = true; createControl.close(); }
         }
         this.preview();
     },
@@ -62,24 +62,24 @@ var create = {
         app.eventRemover("window", "mousemove", this.mousemove);
         app.eventRemover("window", "mouseup", this.mouseup);
         if (this.firstPoint) { this.end(); }
-        screenCorrection.run(app.canvas.canvasToClient(this.object.getLastPoint()), function () { app.redraw(); create.preview();});
+        screenCorrection.run(app.canvas.canvasToClient(this.object.getLastPoint()), function () { create.preview(); });
     },
     end: function () {
-        if (!this.object) { return;}//اگر این تابع بی دلیل در تغییر مود برنامه صدا زده شد ادامه نده
+        if (this.drawing === false) { return;} // drawing = false is mean that current drawing is saved
         this.firstPoint = true;
         createControl.close();
         this.save();
-        this.object = undefined;
         app.redraw();
     },
     preview: function () {
-        var points = this.object.getPoints(), lines = this.object.getLines();
-        app.canvas.clear();
         app.redraw();
-        for (var i = 0; i < points.length; i++) { app.drawPoint(points[i]); }
-        for (var i = 0; i < lines.length; i++) { app.drawLine($.extend({}, lines[i], {showDimension:i===lines.length - 1})); }
-        this.drawLastPoint();
-        this.drawController();
+        if (this.drawing) {
+            var points = this.object.getPoints(), lines = this.object.getLines();
+            for (var i = 0; i < points.length; i++) { app.drawPoint(points[i]); }
+            for (var i = 0; i < lines.length; i++) { app.drawLine($.extend({}, lines[i], { showDimension: i === lines.length - 1 })); }
+            this.drawLastPoint();
+            this.drawController();
+        }
     },
     drawLastPoint: function () {
         var point = this.object.state.points[this.object.state.points.length - 1];
@@ -87,6 +87,7 @@ var create = {
         app.canvas.drawArc({ x: point.x, y: point.y, radius: 6, stroke: "orange" });
     },
     drawController: function () {
+        if (this.firstPoint === true) { return;}
         var o = this.object, points = o.getPoints(), lines = o.getLines(), lastPoint = points[points.length - 1];
         var control = { end: true, keyboard: true, move: true, pan: true };
         if (o.getMode() === "polyline") {
@@ -97,7 +98,7 @@ var create = {
         control.coords = { x: lastPoint.x, y: lastPoint.y };
         createControl.open(control);
     },
-    drawcontrolremove: function () { this.object.state.points.pop(); screenCorrection.run(app.canvas.canvasToClient(this.object.getLastPoint()), function () { app.redraw(); create.preview(); }); },
+    drawcontrolremove: function () { this.object.state.points.pop(); screenCorrection.run(app.canvas.canvasToClient(this.object.getLastPoint()), function () { create.preview(); }); },
     drawcontrolclose: function () { this.object.close(); this.end(); },
     drawcontroljoin: function () { this.object.join(); this.end(); },
     drawcontrolmove: function (e) {
@@ -115,7 +116,7 @@ var create = {
     movemouseup: function () {
         app.eventRemover("window", "mousemove", this.movemousemove);
         app.eventRemover("window", "mouseup", this.movemouseup);
-        screenCorrection.run(app.canvas.canvasToClient(create.object.getLastPoint()), function () { app.redraw(); create.preview(); });
+        screenCorrection.run(app.canvas.canvasToClient(create.object.getLastPoint()), function () { create.preview(); });
     },
     drawcontrolend: function () { this.end(); },
     drawcontrolpan: function (e) {
@@ -143,35 +144,35 @@ var create = {
             negative: true,
             title: mode + " to:",
             close: (mode === "ngon" || mode === "rectangle" || mode === "frame"),
-            callback: function(obj){
+            callback: function (obj) {
                 var lastPoint = create.object.getLastPoint();
-                var newPoint = {x:lastPoint.x + obj.x,y:lastPoint.y+obj.y*-1};
+                var newPoint = { x: lastPoint.x + obj.x, y: lastPoint.y + obj.y * -1 };
                 create.object.to(newPoint);
-                screenCorrection.run(app.canvas.canvasToClient(create.object.getLastPoint()), function () { app.redraw(); create.preview(); });
+                screenCorrection.run(app.canvas.canvasToClient(create.object.getLastPoint()), function () { create.preview(); });
             }
         });
     },
     save: function () {
-        this.drawing=false;
         var points = this.object.getPoints(), lines = this.object.getLines();
-        if (lines.length === 0) { return;}
+        if (lines.length === 0) { return; }
         var addedPoints = [], addedLines = [];
-        for (var i = 0; i < points.length; i++) { 
+        for (var i = 0; i < points.length; i++) {
             var addedPoint = Points.add(points[i]); addedPoints.push(addedPoint);
-            if(addedLines[i - 1]){
+            if (addedLines[i - 1]) {
                 addedLines[i - 1].end.id = addedPoint.id;
-                addedPoint.connectedLines.push({side:"end",id:addedLines[i - 1].id});
+                addedPoint.connectedLines.push({ side: "end", id: addedLines[i - 1].id });
             }
-            if(lines[i]){
+            if (lines[i]) {
                 var addedLine = Lines.add(lines[i]); addedLines.push(addedLine);
-                addedPoint.connectedLines.push({side:"start",id:addedLine.id});
+                addedPoint.connectedLines.push({ side: "start", id: addedLine.id });
                 addedLine.start.id = addedPoint.id;
-                if(i === points.length - 1){
+                if (i === points.length - 1) {
                     addedLine.end.id = addedPoints[0].id;
-                    addedPoints[0].connectedLines.push({side:"end",id:addedLine.id});
+                    addedPoints[0].connectedLines.push({ side: "end", id: addedLine.id });
                 }
             }
         }
+        this.drawing = false;
     },
     getPoints: {
         polyline: function () {
@@ -207,7 +208,7 @@ var create = {
             return points;
         }
     },
-    settingInstance:null,
+    settingInstance: null,
     setting: function () {
         var template = [
             {
@@ -220,17 +221,17 @@ var create = {
         ];
         if (app.state.createmode === "ngon") {
             template.push({
-                type: "slider", title: "Sides",value: create.ngonSides,
+                type: "slider", title: "Sides", value: create.ngonSides,
                 callback: function (value) { create.ngonSides = value; },
                 start: 3, step: 1, end: 40,
             });
             template.push({
-                type:"switch",title: "Ortho", value: create.ortho,
+                type: "switch", title: "Ortho", value: create.ortho,
                 callback: function (value) { create.ortho = value; },
             });
         }
         Alert.open({
-            buttons: [{ text: "ok",callback:Alert.close }],
+            buttons: [{ text: "ok", callback: Alert.close }],
             template: template,
             title: app.state.createmode + " setting.",
         });
@@ -238,16 +239,16 @@ var create = {
 }
 
 var autoPan = {
-    moving:false,
+    moving: false,
     margin: { left: 40, top: 40, right: 40, bottom: 40 },
     run: function (coords, callback) {
-        
+
         var c = app.canvas;
         if (this.moving || !c.getIsDown()) { return; }
         var speed = 2, x = coords.x, y = coords.y, m = this.margin;
-        if (x > c.getWidth() - m.right) { var h = 1; }else if (x < m.left) { var h = -1; }else { var h = 0; }
-        if (y > c.getHeight() - m.bottom) { var v = -1; }else if (y < m.top) { var v = 1; }else { var v = 0; }
-        
+        if (x > c.getWidth() - m.right) { var h = 1; } else if (x < m.left) { var h = -1; } else { var h = 0; }
+        if (y > c.getHeight() - m.bottom) { var v = -1; } else if (y < m.top) { var v = 1; } else { var v = 0; }
+
         if (v || h) {
             this.moving = true;
             setTimeout(function () {
@@ -261,8 +262,8 @@ var autoPan = {
 
 
 var screenCorrection = {
-    margin: { left: 80+0, top: 80+36, right: 80+0, bottom: 80+0 },//80 is createControl.style.distance
-    run: function (coords,callback) {
+    margin: { left: 80 + 0, top: 80 + 36, right: 80 + 0, bottom: 80 + 0 },//80 is createControl.style.distance
+    run: function (coords, callback) {
         var c = app.canvas;
         var speed = 2, x = coords.x, y = coords.y, m = this.margin, width = c.getWidth(), height = c.getHeight();
         if (x > width - m.right) { var deltaX = x - width + m.right; }
@@ -271,6 +272,6 @@ var screenCorrection = {
         if (y > height - m.bottom) { var deltaY = y - height + m.bottom; }
         else if (y < m.top) { var deltaY = y - m.top; }
         else { var deltaY = 0; }
-        c.setScreenBy({x: deltaX, y: deltaY * -1, animate: true, callback: callback });
+        c.setScreenBy({ x: deltaX, y: deltaY * -1, animate: true, callback: callback });
     }
 }
